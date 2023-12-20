0378ca1d4daded21ef3738c069b02388ab8c07d6
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-12-20 20:00:48 +0200

    revert fixing errors

--- src/Shared/MasterLayout/MasterLayout.tsx
+++ src/Shared/MasterLayout/MasterLayout.tsx
@@ -1,30 +1,47 @@
-import { Outlet } from "react-router-dom";
-import Header from "../Header/Header";
-import NavBar from "../Navbar/NavBar";
-import SideBar from "../SideBar/SideBar";
 
-export default function MasterLayout({ userData }: any) {
+
+
+import { Outlet } from 'react-router-dom'
+import Header from '../Header/Header'
+import NavBar from '../Navbar/NavBar'
+import SideBar from '../SideBar/SideBar'
+
+export default function MasterLayout({userData}:any) {
   return (
-    <>
-      <div className="container-fluid master">
-        <div className="col-md-12 bg-white">
-          <NavBar userData={userData} />
-        </div>
+ <>
 
-        <div className="d-flex">
-          <div className="col-md-2 vh-100">
-            <div className="d-flex ">
-              <div className="col-md-2 ">
-                <SideBar />
-              </div>
-              <div className="col-md-10 dashboard mt-0">
-                {/* <Header /> */}
-                <Outlet userData={userData} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+<div className="container-fluid master">
+    <div className="col-md-12 bg-white  ">
+        <NavBar userData={userData}/>
+    </div>
+
+    <div className='d-flex'>
+    <div className="col-md-2 vh-100  ">
+
+    <div className='d-flex '>
+    <div className="col-md-2 "  >
+
+        <SideBar/>
+        
+    </div>
+    <div className='col-md-10 dashboard mt-0 '>
+
+      
+    {/* <Header /> */}
+      
+
+    <Outlet userData={userData}/>
+
+
+
+    </div>
+    </div>
+  
+</div>
+   
+    
+  
+
     </>
-  );
+  )
 }
