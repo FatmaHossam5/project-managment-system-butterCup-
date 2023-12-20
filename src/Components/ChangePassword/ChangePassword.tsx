f8b9b74fc5f1ab6a01765b21c274308fee3f34e4
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-12-20 19:47:23 +0200

    fixing errors

--- src/Components/ChangePassword/ChangePassword.tsx
+++ src/Components/ChangePassword/ChangePassword.tsx
@@ -1,9 +1,9 @@
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import {  toast } from "react-toastify";
+import { toast } from "react-toastify";
 
-export default function ChangePass({ handleClose }:any) {
+export default function ChangePass({ handleClose }: any) {
   const navigate = useNavigate();
 
   const {
@@ -12,19 +12,15 @@ export default function ChangePass({ handleClose }:any) {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data:any) => {
+  const onSubmit = (data: any) => {
     // console.log(data);
 
     axios
-      .put(
-        "https://upskilling-egypt.com/api/v1/Users/ChangePassword",
-        data,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
-          },
-        }
-      )
+      .put("https://upskilling-egypt.com/api/v1/Users/ChangePassword", data, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+        },
+      })
       .then((response) => {
         handleClose();
         toast.success("password changed successsfully", {
@@ -54,93 +50,87 @@ export default function ChangePass({ handleClose }:any) {
 
   return (
     <>
-     <div className="Auth-container container-fluid ">
- 
+      <div className="Auth-container container-fluid ">
+        <div className="row bg-overlay justify-content-center align-items-center">
+          <div className="row bg-overlay vh-100 justify-content-center align-items-center">
+            <div className="col-lg-5 col-md-7 col-sm-9 ">
+              <form className="w-75 m-auto" onSubmit={handleSubmit(onSubmit)}>
+                <h4 className="fw-bolder color position-relative p-0">
+                  Change Your Password
+                </h4>
+                <div className="form-group my-1 position-relative">
+                  <p className="color my-0" style={{ fontSize: "14px" }}>
+                    Old Password
+                  </p>
+                  <input
+                    placeholder="Old Password"
+                    className="form-control ps-4 mb-1"
+                    type="password"
+                    {...register("oldPassword", {
+                      required: true,
+                    })}
+                  />
+                  <hr className="text-white" />
 
- <div className="row bg-overlay justify-content-center align-items-center">
+                  {errors.oldPassword &&
+                    errors.oldPassword.type === "required" && (
+                      <span className="text-danger">
+                        oldPassword is required
+                      </span>
+                    )}
+                </div>
+                <div className="form-group my-1 position-relative">
+                  <p className="color my-0" style={{ fontSize: "14px" }}>
+                    New Password
+                  </p>
 
- <div className="row bg-overlay vh-100 justify-content-center align-items-center">
+                  <input
+                    placeholder="New Password"
+                    className="form-control ps-4 mb-1"
+                    type="password"
+                    {...register("newPassword", {
+                      required: true,
+                    })}
+                  />
+                  <hr className="text-white" />
 
-   <div className="col-lg-5 col-md-7 col-sm-9 ">
+                  {errors.newPassword &&
+                    errors.newPassword.type === "required" && (
+                      <span className="text-danger">
+                        newPassword is required
+                      </span>
+                    )}
+                </div>
+                <div className="form-group my-1 position-relative">
+                  <p className="color my-0" style={{ fontSize: "14px" }}>
+                    Confirm Password
+                  </p>
+                  <input
+                    placeholder="Confirm New Password"
+                    className="form-control ps-4 mb-1"
+                    type="password"
+                    {...register("confirmNewPassword", {
+                      required: true,
+                    })}
+                  />
+                  <hr className="text-white" />
 
-   
+                  {errors.confirmNewPassword &&
+                    errors.confirmNewPassword.type === "required" && (
+                      <span className="text-danger">
+                        confirmNewPassword is required
+                      </span>
+                    )}
+                </div>
 
-      
-         <form className="w-100 m-auto" onSubmit={handleSubmit(onSubmit)}>
-
-         <div className="form-group from-design py-3 my-5 rounded-2  ">
-      
-         <form className="w-75 m-auto" onSubmit={handleSubmit(onSubmit)}>
-
-              <h4 className="fw-bolder color position-relative p-0">Change Your Password</h4>
-              <div className="form-group my-1 position-relative">
-               <p  className="color my-0" style={{ fontSize: '14px' }}>Old Password</p>
-                <input
-                  placeholder="Old Password"
-                  className="form-control ps-4 mb-1"
-                  type="password"
-                  {...register("oldPassword", {
-                    required: true,
-                  })}
-                />
-                    <hr className="text-white" />
-
-                {errors.oldPassword &&
-                  errors.oldPassword.type === "required" && (
-                    <span className="text-danger">oldPassword is required</span>
-                  )}
-              </div>
-              <div className="form-group my-1 position-relative">
-                <p className="color my-0" style={{ fontSize: '14px' }}>New Password</p>
-
-                <input
-                  placeholder="New Password"
-                  className="form-control ps-4 mb-1"
-                  type="password"
-                  {...register("newPassword", {
-                    required: true,
-                  })}
-                />
-                 <hr className="text-white" />
-
-                {errors.newPassword &&
-                  errors.newPassword.type === "required" && (
-                    <span className="text-danger">newPassword is required</span>
-                  )}
-              </div>
-              <div className="form-group my-1 position-relative">
-                <p className="color my-0" style={{ fontSize: '14px' }}>Confirm Password</p>
-                <input
-                  placeholder="Confirm New Password"
-                  className="form-control ps-4 mb-1"
-                  type="password"
-                  {...register("confirmNewPassword", {
-                    required: true,
-                  })}
-                />
-                   <hr className="text-white" />
-
-                {errors.confirmNewPassword &&
-                  errors.confirmNewPassword.type === "required" && (
-                    <span className="text-danger">
-                      confirmNewPassword is required
-                    </span>
-                  )}
-              </div>
-
-              <div className="form-group my-3">
-                <button className="btn  w-100">Change Password</button>
-              </div>
-            </form>
-     
-     </div>
-   </div>
-
- 
-
- </div>
-</div>
-
+                <div className="form-group my-3">
+                  <button className="btn  w-100">Change Password</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/Shared/SideBar/SideBar.tsx
+++ src/Shared/SideBar/SideBar.tsx
@@ -1,6 +1,6 @@
-import  { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Sidebar, Menu, MenuItem} from "react-pro-sidebar";
+import { Sidebar as ReactSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import Modal from "react-bootstrap/Modal";
 import ChangePassword from "../../Components/ChangePassword/ChangePassword";
 
@@ -14,143 +14,47 @@ export default function SideBar() {
   //   setIsCollapsed(!isCollapsed);
   // };
   let navigate = useNavigate();
- 
 
   return (
     <div className="sidebar-container">
       <Modal show={show} onHide={handleClose}>
-      
         <Modal.Body>
-          <ChangePassword handleClose={handleClose}/>
+          <ChangePassword handleClose={handleClose} />
         </Modal.Body>
-        
       </Modal>
-      <Sidebar collapsed={false} className="p-0 m-0" toggled={true}>
+      <ReactSidebar collapsed={false} className="p-0 m-0" toggled={true}>
         <Menu>
-          
           <MenuItem
-           style={{ fontSize: '12px' }} 
-           icon={<i className="fa fa-home" aria-hidden="true"></i>}
-           component={<Link to="/dashboard" />}
+            style={{ fontSize: "12px" }}
+            icon={<i className="fa fa-home" aria-hidden="true"></i>}
+            component={<Link to="/dashboard" />}
           >
             Home
           </MenuItem>
           <MenuItem
-           style={{ fontSize: '12px' }} 
+            style={{ fontSize: "12px" }}
             icon={<i className="fa fa-users" aria-hidden="true"></i>}
             component={<Link to="/dashboard/users" />}
           >
             Users
           </MenuItem>
-         
+
           <MenuItem
-           style={{ fontSize: '12px' }} 
+            style={{ fontSize: "12px" }}
             icon={<i className="fa-solid fa-bars"></i>}
             component={<Link to="/dashboard/categories" />}
           >
             Tasks
           </MenuItem>
           <MenuItem
-          style={{ fontSize: '12px' }}  onClick={handleShow}
+            style={{ fontSize: "12px" }}
+            onClick={handleShow}
             icon={<i className="fa fa-key" aria-hidden="true"></i>}
           >
             Change Password
           </MenuItem>
-        
         </Menu>
-      </Sidebar>
+      </ReactSidebar>
     </div>
-
   );
 }
-
-import { AuthContext } from "../../Context/AuthContext";
-
-export default function SideBar() {
-  let [isCollapsed, setIsCollapsed] = useState(false);
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  let handleToggle = () => {
-    setIsCollapsed(!isCollapsed);
-  };
-  let navigate = useNavigate();
- let {role}:any=useContext(AuthContext)
- let logOut =()=>{
-  localStorage.removeItem("userToken")
-  navigate('/login')
-} 
-console.log(role);
-
- 
-  return (
-    <>
- 
-  
-   
-    <Modal show={show} onHide={handleClose}>
-      
-      <Modal.Body>
-        <ChangePassword handleClose={handleClose}/>
-      </Modal.Body>
-      
-   </Modal>
-    <div style={{ display: 'flex', height: '100%', minHeight: '400px' }}>
-      <Sidebar collapsed={!isCollapsed} >
-        <Menu>
-        <MenuItem></MenuItem>
-        <MenuItem
-           style={{ fontSize: '12px' }} 
-           icon={<i className="fa fa-home fa-2x ico" aria-hidden="true"></i>}
-           component={<Link to="/dashboard" />}>
-            Home
-            
-          </MenuItem>
-          {role==='Manager'?<MenuItem
-           style={{ fontSize: '12px' }} 
-            icon={<i className="fa fa-users fa-2x ico" aria-hidden="true"></i>}
-            component={<Link to="/dashboard/users" />}
-          >
-            Users
-          </MenuItem>  :''}
-        
-          <MenuItem
-           style={{ fontSize: '12px' }} 
-            icon={<i className="fa-solid fa-diagram-project fa-2x ico"></i>}
-            component={<Link to="/dashboard/projects" />}
-          >
-           Projects
-          </MenuItem>
-          <MenuItem
-           style={{ fontSize: '12px' }} 
-            icon={<i className="fa-solid fa-list-check fa-2x ico"></i>}
-            component={<Link to="/dashboard/tasks" />}
-          >
-           Tasks
-          </MenuItem>
-          <MenuItem
-          style={{ fontSize: '12px' }}            onClick={handleShow}
-            icon={<i className="fa fa-key fa-2x ico " aria-hidden="true"></i>}
-          >
-            Change Password
-          </MenuItem>
-          <MenuItem   style={{ fontSize: '12px' }} 
-           icon={<i className="fa-solid fa-arrow-right-from-bracket ico fa-2x"></i> } onClick={logOut}> logOut </MenuItem>
-        </Menu>
-      </Sidebar>
-      <main style={{ padding: 10 }}>
-        <div>
-          <div className=" position-relative arrow " onClick={() => setIsCollapsed(!isCollapsed)}>
-          {isCollapsed? <i className="fa-solid fa-chevron-left text-white position-absolute"></i>:<i className="fa-solid fa-chevron-right text-white coll position-absolute"></i>}
-         
-       
-          </div>
-        </div>
-      </main>
-
-    
-    </div>
-    </>
-  );
-}  
-
